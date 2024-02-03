ef43e8b88eab8254c2e16093518197e855bb04b2
RyanWMHI <ryan@fedml.ai>
2024-02-03 10:53:16 +0800

    perf: bugfix npm run build 2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue2";
-// import Sitemap from "vite-plugin-sitemap";
+import Sitemap from "vite-plugin-sitemap";
 import Uvc from "unplugin-vue-components/vite";
 import { BootstrapVueResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
@@ -49,16 +49,18 @@ const config = defineConfig(({ command, mode }) => {
           BootstrapVueResolver()
         ]
       }),
-      // Sitemap({
-      //   hostname: env.VUE_APP_BASE_API,
-      //   dynamicRoutes: [
-      //     "/",
-      //     "/maodina",
-      //     "/researchPapers",
-      //     "/contact",
-      //     "/press"
-      //   ],
-      // }),
+      Sitemap({
+        hostname: env.VUE_APP_BASE_API,
+        dynamicRoutes: [
+          "/",
+          "/maodina",
+          "/researchPapers",
+          "/contact",
+          "/press",
+          "/projectOverview",
+          "/maodian",
+        ],
+      }),
     ],
   };
 });
